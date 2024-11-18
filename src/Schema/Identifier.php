05b74b2500569e2718c14367d0a7af1680f68d17
Sergei Morozov <morozov@tut.by>
2024-11-18 13:32:49 -0800

    Remove unused Identifier constructor parameterm $quoted

--- src/Schema/Identifier.php
+++ src/Schema/Identifier.php
@@ -14,18 +14,4 @@ namespace Doctrine\DBAL\Schema;
  */
 class Identifier extends AbstractAsset
 {
-    /**
-     * @param string $identifier Identifier name to wrap.
-     * @param bool   $quote      Whether to force quoting the given identifier.
-     */
-    public function __construct(string $identifier, bool $quote = false)
-    {
-        parent::__construct($identifier);
-
-        if (! $quote || $this->_quoted) {
-            return;
-        }
-
-        $this->_setName('"' . $this->getName() . '"');
-    }
 }
